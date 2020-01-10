905c14d99150b4644119c0cd0e840eabd8d0792c
Peter Havens <peter.havens@gmail.com>
2020-01-10 11:35:09 -0700

    Add solution to "Java String Reverse"

---
+++ java/java-string-reverse/Solution.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        System.out.println(new StringBuilder(A).reverse().toString().equals(A) ? "Yes" : "No");
+    }
+}
